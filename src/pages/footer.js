78b65136c04bcfb1098dc7ca545ff4fd603d2e4f
felipe.silva <felipe.silva@grupomultiplica.com.br>
2022-03-02 09:53:11 -0300

    att

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -8,13 +8,11 @@ function footer(){
 				<h3>CONTATO</h3>
 				<span>avenida testando a apaciencia</span>
 			</div>
-			<div>
+			<div className="divMapa">
 				<iframe
-					title="mapa"
-					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3653.9866281493664!2d-46.68045448501989!3d-23.67643618462587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce501ac3b77f8f%3A0xdaa230261180506a!2sShopping%20Interlagos!5e0!3m2!1spt-BR!2sbr!4v1646176699707!5m2!1spt-BR!2sbr"
-					allowFullScreen
+					className="mapa" title="mapa"
+					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3653.9866281493664!2d-46.68045448501989!3d-23.67643618462587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce501ac3b77f8f%3A0xdaa230261180506a!2sShopping%20Interlagos!5e0!3m2!1spt-BR!2sbr!4v1646225363122!5m2!1spt-BR!2sbr"
 					loading="lazy"
-					className="mapa"
 				></iframe>
 			</div>
 		</div>
