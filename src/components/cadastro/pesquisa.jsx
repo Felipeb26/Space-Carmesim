5f46acd507504481c6029a2494ae7790bfc5d196
Felipeb26 <felipeb2silva@hotmail.com>
2022-04-03 10:41:32 -0300

    att

--- src/components/cadastro/pesquisa.jsx
+++ src/components/cadastro/pesquisa.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ORUMAITO from "assets/img/orumaito.png";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const customId = "custom-id-yes";
